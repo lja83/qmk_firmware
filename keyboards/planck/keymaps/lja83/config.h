496432edf491f4aff399beafc62158c3699f2331
Jeff Apczynski <jeffrey.apczynski@braincorp.com>
2021-05-08 20:35:10 -0700

    add clicks

--- keyboards/planck/keymaps/lja83/config.h
+++ keyboards/planck/keymaps/lja83/config.h
@@ -34,3 +34,11 @@
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
 
+#define AUDIO_CLICKY
+// #ifdef AUDIO_CLICKY
+//     #define AUDIO_CLICKY_FREQ_DEFAULT 261.63f
+//     #define AUDIO_CLICKY_FREQ_MIN 65.41f
+//     #define AUDIO_CLICKY_FREQ_MAX 1046.5f
+//     #define AUDIO_CLICKY_FREQ_FACTOR 1.18921f
+//     #define AUDIO_CLICKY_FREQ_RANDOMNESS 17.0f
+// #endif
